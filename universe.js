142975289f9dad18720af8a995998d5609c2aa0f
macsandoval1999 <msandoval2@byupathway.edu>
2026-02-03 23:16:43 -0700

    Initialized Repository after working out base starting point for final project

---
+++ universe.js
@@ -0,0 +1,14 @@
+function createStars() {
+    const universe = document.querySelector("body");
+    for (let i = 0; i < 1000; i++) {
+        const star = document.createElement("div");
+        star.classList.add("star");
+        star.style.top = `${Math.random() * 100}%`;
+        star.style.left = `${Math.random() * 100}%`;
+        star.style.width = `${Math.random() * 2}px`;
+        star.style.height = star.style.width;
+        star.style.animationDuration = `${Math.random() * 3 + 2}s`;
+        universe.appendChild(star);
+    }
+}
+createStars();
