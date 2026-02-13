341b1a6186d950e281ec8a3dfb199bf6f3a63ad5
macsandoval1999 <msandoval2@byupathway.edu>
2026-02-12 22:57:13 -0700

    attempt to fix bug after back stepping into index page from planet page. Page solar system was stuck since it was still in animation aftermath. Changed code to refresh if event is still persisting

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 import { initSolarSystem } from "./solarSystem.mjs";
 import { loadPageMutuals } from "./utils.mjs";
 
+window.addEventListener("pageshow", (event) => {
+    if (event.persisted) {
+        window.location.reload();
+    }
+});
+
 initSolarSystem();
 loadPageMutuals();
